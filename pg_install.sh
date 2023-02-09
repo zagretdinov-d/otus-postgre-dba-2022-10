ac422825d4c07d90cf3fc2a8ff50a8a4dba27b19
zagretdinov-d <85208391+zagretdinov-d@users.noreply.github.com>
2023-02-09 22:17:52 +0600

    Delete pg_install.sh

--- pg_install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y -q && sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt -y install postgresql-14
